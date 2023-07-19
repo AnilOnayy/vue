7b456ecd956d3302098011d47759a174601ae786
Anıl Onay <cengizhananilonay@gmail.com>
2023-07-19 10:11:09 +0300

    Component Slots.

---
+++ 12-component-slots/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- <ModelComponent :onSave="onSave"  :title="'Model Title'" :content=" 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente in eos expedita sunt aliquam laboriosam ipsa! Aspernatur, odit rem quaerat amet, velit facilis explicabo ullam recusandae, minus quisquam earum in.' "  /> -->
+ 
+  <ModelComponent>
+      <!-- <h3>Title info from slots</h3>  -->
+
+      <template #title>
+          <h3>Title info from slots</h3>
+      </template>
+<!-- 
+      <template #content>
+         <p class="text-green">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos excepturi veritatis distinctio dolorum eius velit laboriosam accusamus voluptatibus cupiditate vero.
+         </p>
+      </template> -->
+
+      <template v-slot:content>
+         <p class="text-green">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos excepturi veritatis distinctio dolorum eius velit laboriosam accusamus voluptatibus cupiditate vero.
+         </p>
+      </template>
+
+      <template #default>
+        This will be show by default.
+      </template>
+  </ModelComponent>
+</template>
+
+
+<script>
+  import ModelComponent from './components/ModelComponent.vue';
+
+  export default {
+
+    
+    methods : {
+        onSave(){
+            alert ( "Saved!!");
+        }
+    },
+
+    components:{
+      ModelComponent
+    }
+  }
+
+</script>
---
+++ 12-component-slots/src/components/ModelComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="modal container">
+        <div class="header">
+            <!-- <h1>{{ title }}</h1> -->
+            <!-- <slot/>  -->
+            <slot name="title" /> 
+        </div>
+        <hr />
+        <div class="content my-2">
+            <slot name="content" />
+        </div>
+        <slot />
+        <hr />
+        <div class="footer text-right">
+            <button class="me-2">Kapat</button>
+            <button  @click="onSave" class="green">Kaydet</button>
+        </div>
+    </div>
+   
+</template>
+
+
+<script>
+export default {
+    // props : ["onSave","title","content"],
+    
+    
+}
+</script>
