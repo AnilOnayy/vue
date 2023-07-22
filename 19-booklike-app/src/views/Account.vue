ab70dc3e56d5d15513cb26aaad0ba2c6dcd6398d
Anıl Onay <cengizhananilonay@gmail.com>
2023-07-22 20:52:06 +0300

    Like,Bookmark Services  Added , Account Page Started

---
+++ 19-booklike-app/src/views/Account.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-row">
+        <side-bar />
+        <AppBookmarkList :bookmarkList="bookmarkList" />
+    </div>
+</template>
+
+
+
+<script>
+import SideBar from '@/components/Account/SideBar.vue'
+import AppBookmarkList from '@/components/Shared/AppBookmarkList';
+export default {
+    data() {
+        return {
+            bookmarkList: []
+        }
+    },
+
+    created() {
+        this.$appAxios.get(`/bookmarks?_expand=category&_expand=user&categoryId=${categoryId}`)
+        .then(response => {
+            this.bookmarkList = response?.data || [];
+        })
+    }
+
+    components: {
+        SideBar,
+        AppBookmarkList
+    }
+}
+</script>
